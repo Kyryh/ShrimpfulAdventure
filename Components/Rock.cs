39167064fe73f84c2a6e250eb5a9e76377e5e503
Kyryh <71778401+Kyryh@users.noreply.github.com>
2024-08-20 09:52:17 +0200

    Create Rock.cs

---
+++ Components/Rock.cs
@@ -0,0 +1,29 @@
+﻿using KEngine;
+using KEngine.Components;
+using KEngine.Components.Colliders;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShrimpfulAdventure.Components {
+    internal class Rock : Component {
+        float yVelocity = 0f;
+        public override void Initialize() {
+            base.Initialize();
+            GameObject.GetComponent<Collider>().OnCollision += Rock_OnCollision;
+        }
+
+        private void Rock_OnCollision(Collider other, Collider.HitInfo hitInfo) {
+            if (hitInfo.direction == GameConstants.Vector2.Down)
+                yVelocity = 0;
+        }
+
+        public override void Update(float deltaTime) {
+            yVelocity -= 0.01f;
+            Transform.Position += new Vector2(0, yVelocity);
+        }
+    }
+}
