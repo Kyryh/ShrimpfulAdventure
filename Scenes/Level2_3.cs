402968048b2566a465fdbb47825c3d2693469ff9
Kyryh <71778401+Kyryh@users.noreply.github.com>
2024-08-20 15:08:11 +0200

    Level 2-3

--- Scenes/Level2_3.cs
+++ Scenes/Level2_3.cs
@@ -12,43 +12,27 @@ namespace ShrimpfulAdventure.Scenes {
 
             MapLoader.LoadMap("2-3", "Sprites/tilesets1");
 
-            MapLoader.SpawnShrimps(new Vector2(3.5f, 0f));
+            MapLoader.SpawnShrimps(new Vector2(1.5f, -22.5f));
 
+            var td1 = MapLoader.SpawnTrapdoor(new Vector2(20.5f, -16), MathHelper.PiOver2, false, true);
 
-            //var td1 = MapLoader.SpawnTrapdoor(new Vector2(5.5f, -11.75f), -MathHelper.PiOver2, true);
-            //var td2 = MapLoader.SpawnTrapdoor(new Vector2(7f, -9.5f), MathHelper.Pi, false, true);
-            //var td3 = MapLoader.SpawnTrapdoor(new Vector2(21.5f, -5.75f), MathHelper.PiOver2, false);
+            var td2 = MapLoader.SpawnTrapdoor(new Vector2(28.75f, -3.5f), 0f, false, true);
 
-            //MapLoader.SpawnLever(new Vector2(20.5f, -5f),
-            //    () => {
-            //        td1.Switch();
-            //        td2.Switch();
-            //        td3.Switch();
-            //    },
-            //    () => {
-            //        td1.Switch();
-            //        td2.Switch();
-            //        td3.Switch();
-            //    }
-            //);
+            MapLoader.SpawnLever(new Vector2(34.5f, -15f),
+                () => {
+                    td1.Switch();
+                    td2.Switch();
+                },
+                () => {
+                    td1.Switch();
+                    td2.Switch();
+                }
+            );
 
-            //var td4 = MapLoader.SpawnTrapdoor(new Vector2(15.5f, -17.75f), MathHelper.PiOver2, true);
-
-
-            //MapLoader.SpawnLever(new Vector2(10.5f, -17), td4.Switch, td4.Switch);
-
-            //MapLoader.SpawnRock(new Vector2(14.5f, -15.5f));
-
-            //var td5 = MapLoader.SpawnTrapdoor(new Vector2(31.25f, -19.5f), MathHelper.Pi, true);
-
-            //MapLoader.SpawnButton(new Vector2(7.5f, -23.30f), td5.Switch, td5.Switch);
-
-
-            //MapLoader.SpawnCurrent(new Vector2(35.5f, -23.5f), 24, 5, 0f);
 
             MapLoader.CreateBackground("Sprites/background3");
 
-            //MapLoader.AddEndTrigger("1-3", new Vector2(35.5f, 0), new Vector2(8, 1));
+            MapLoader.AddEndTrigger("3-1", new Vector2(41f, -13f), new Vector2(1, 3));
         }
     }
 }
